bfe4b56c491a5c27d1c16360364692fd11a187eb
waltodd <waltodd@hotmail.com>
2024-09-04 02:05:27 +0100

    add turbo repo

---
+++ apps/electron-app/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+
+    mainWindow.loadURL(
+        process.env.ELECTRON_START_URL || `file://${path.join(__dirname, '../build/index.html')}`
+    );
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
